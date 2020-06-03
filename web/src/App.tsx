3b74a712078b5baff22ac5dcc0d9ebafbf3f2b1a
Melquias Ribeiro <melquias10.mr@gmail.com>
2020-06-03 13:31:52 -0300

    feat: initial structure and styles

---
+++ web/src/App.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+
+import Header from './components/Header/index'
+import Home from './pages/Home/index';
+
+function App() {
+  return (
+    <>
+     <Home/>
+    </>
+  );
+}
+
+export default App;
---
+++ web/src/components/Header/index.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const Headaer: React.FC = () => {
+    return (
+        <header>
+            <h1>Ecoleta</h1>
+        </header>
+    )
+}
+export default Headaer;
