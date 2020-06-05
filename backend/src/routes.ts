32258d692dfd7f427e1ddbdda92ee690745168b1
Melquias Ribeiro <melquias10.mr@gmail.com>
2020-06-05 17:36:21 -0300

    feat: validing data to register point

--- backend/src/routes.ts
+++ backend/src/routes.ts
@@ -1,5 +1,6 @@
 import express from 'express';
 import multer from 'multer';
+import { celebrate, Joi } from 'celebrate';
 
 import multerConfig from './config/multer';
 import PointsController from './controllers/pointsController';
@@ -13,7 +14,28 @@ const itensController = new ItensController();
 
 routes.get('/itens', itensController.index);
 
-routes.post('/point', upload.single('image'), pointController.create);
+routes.post(
+  '/point',
+  upload.single('image'),
+  celebrate(
+    {
+      body: Joi.object().keys({
+        name: Joi.string().required(),
+        email: Joi.string().required().email(),
+        whatsapp: Joi.number().required(),
+        latitude: Joi.number().required(),
+        longitude: Joi.number().required(),
+        city: Joi.string().required(),
+        uf: Joi.string().required().max(2),
+        itens: Joi.string().required(),
+      }),
+    },
+    {
+      abortEarly: false,
+    },
+  ),
+  pointController.create,
+);
 routes.get('/point', pointController.index);
 routes.get('/point/:id', pointController.show);
 
--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,6 +1,7 @@
 import express from 'express';
 import cors from 'cors';
 import path from 'path';
+import { errors } from 'celebrate';
 import routes from './routes';
 
 const app = express();
@@ -10,5 +11,7 @@ app.use(express.json()); // config para aceiatr json
 app.use(routes); // rotas do app
 
 app.use('/uploads', express.static(path.resolve(__dirname, '..', 'uploads')));
+
+app.use(errors());
 
 app.listen(3333); // porta
